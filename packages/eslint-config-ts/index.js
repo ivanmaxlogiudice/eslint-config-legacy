57baa430762849a5be310e438df636a236562530
Iván Lo Giudice <ivanmaxlogiudice@gmail.com>
2023-05-25 16:15:42 -0300

    feat(ts): ban namespace in ts file

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -152,7 +152,6 @@ module.exports = {
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/ban-types': 'off',
-        '@typescript-eslint/no-namespace': 'off',
         '@typescript-eslint/triple-slash-reference': 'off',
 
         // handled by unused-imports/no-unused-imports
